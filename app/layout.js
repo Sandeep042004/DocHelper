7f8781bcd50ef88746bdc7172a85ac57bcd86891
Sandeep042004 <sandy24122004@gmail.com>
2025-09-18 16:29:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,15 @@
 
+import './globals.css'
+
+export const metadata = {
+  title: 'DocDog Chatbot',
+  description: 'Vet Appointment Chatbot',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-100">{children}</body>
+    </html>
+  )
+}
